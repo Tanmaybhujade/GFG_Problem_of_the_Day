5155ea057fa4881d7c69b4a1daaa1894c18b601b
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2022-06-04 19:21:34 +0530

    Create Villain Con.cpp

---
+++ Villain Con.cpp
@@ -0,0 +1,40 @@
+class Solution{
+public:
+    int minColour(int N, int M, vector<int> mat[]) {
+          vector<int> adj[N+1];
+        vector<int> indegree(N+1, 0);
+        for(int i = 0; i < M; i++) {
+            int v = mat[i][1];
+            int u = mat[i][0];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        
+        queue<int> q;
+        for(int i = 1; i <= N; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        int count = 0;
+        while(q.empty() == false) {
+            count++;
+            while(q.empty() == false) {
+                int curr = q.front();
+                q.pop();
+                for(auto it: adj[curr]) {
+                    indegree[it]--;
+                }
+                indegree[curr] = -1;
+            }
+            for(int i = 1; i <= N; i++) {
+                if(indegree[i] == 0) {
+                    q.push(i);
+                }
+            }        
+        }
+    
+        return count;    
+
+    }
+};
