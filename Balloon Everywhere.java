4782ca25cd19a1c115d6a8d325439c6bafe8c6dc
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-02-10 22:07:09 +0530

    Create Balloon Everywhere.java

---
+++ Balloon Everywhere.java
@@ -0,0 +1,9 @@
+class Solution{
+public:
+    int maxInstance(string s){
+        
+      vector<int> ch(26);
+        for(int i=0;i<s.size();i++) ch[s[i]-'a']++;
+        return min({ch[1], ch[0], ch[11]/2, ch[14]/2 ,ch[13]});
+    }
+};
