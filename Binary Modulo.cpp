26cf251e824859e945ad2033cce570bc1980c27d
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-05-08 21:06:13 +0530

    Create Binary Modulo.cpp

---
+++ Binary Modulo.cpp
@@ -0,0 +1,11 @@
+class Solution{
+public:
+    int modulo(string s, int m){
+        int k = 0;
+        for (int i = 0; i < s.length(); i++) {
+            k = k * 2 + (s[i] - '0');
+            k %= m;
+        }
+        return k;
+    }
+};
