9a654a4702ff3157b3deb7380114015669df9f5b
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-07-27 13:14:16 +0530

    Create Multiply two strings.py

---
+++ Multiply two strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def multiplyStrings(self,s1,s2):
+
+        # code here
+
+        # return the product string
+
+        s1 = int(s1)
+
+        s2 = int(s2)
+
+        return s1 * s2
