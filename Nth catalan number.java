0a228070696d72d639d5c3271d283a62921a1e0e
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-08-16 22:16:09 +0530

    Create Nth catalan number.java

---
+++ Nth catalan number.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public static int findCatalan(int n) {
+        int dp[] =new int[n+1];
+        if(n == 0 || n == 1) return 1;
+        dp[0] = 1;
+        dp[1] = 1;
+        int mod= (int)1e9 + 7;
+        for(int i=2; i<=n; i++){
+            for(int j=0; j<i; j++){
+                dp[i] = (dp[i] + (int)( (long)dp[j] * dp[i-1-j]%mod))%mod; 
+                // catalan = catalan%mod;
+            }
+        }
+        return dp[n];
+    }
+}
