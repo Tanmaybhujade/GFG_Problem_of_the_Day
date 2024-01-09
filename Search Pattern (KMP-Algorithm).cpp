416473794c02b5a04d1ca8bddc8a6caaffecec2c
TANMAY BHUJADE <80476913+Tanmaybhujade@users.noreply.github.com>
2024-01-09 22:14:27 +0530

    Create Search Pattern (KMP-Algorithm).cpp

---
+++ Search Pattern (KMP-Algorithm).cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    
+    ArrayList<Integer> search(String pat, String txt)
+    {
+        // your code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        
+        int i=0;
+        
+        while(true){
+            int index=txt.indexOf(pat,i);
+            
+            if(index==-1){
+                break;
+            }
+            else{
+                ans.add(index+1);
+                i=index+1;
+            }
+        }
+        if(ans.size()==0){
+            ans.add(-1);
+        }
+        return ans;
+    }
+}
