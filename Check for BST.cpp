1abd1c092d579e37d42072c6656540a413b23367
TANMAY BHUJADE <80476913+Tanmaybhujade@users.noreply.github.com>
2024-07-24 21:56:14 +0530

    Create Check for BST.cpp

---
+++ Check for BST.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+  public:
+    bool isBSTUtil(Node* node, int min, int max) {
+        if (node == nullptr) {
+            return true;
+        }
+        if (node->data <= min || node->data >= max) {
+            return false;
+        }
+     
+        return isBSTUtil(node->left, min, node->data) && 
+               isBSTUtil(node->right, node->data, max);
+    }
+    bool isBST(Node* root) {
+        return isBSTUtil(root, INT_MIN, INT_MAX);
+    }
+};
