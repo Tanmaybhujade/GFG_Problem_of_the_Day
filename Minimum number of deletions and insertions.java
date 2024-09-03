1e5bd7b27032a653ad4bcd6298d75c2c9bdd0c71
TANMAY BHUJADE <80476913+Tanmaybhujade@users.noreply.github.com>
2024-09-03 22:02:22 +0530

    Create Minimum number of deletions and insertions.java

---
+++ Minimum number of deletions and insertions.java
@@ -0,0 +1,31 @@
+
+class Solution
+{
+	public int minOperations(String str1, String str2) 
+	{ 
+	    // Your code goes here
+	    //this seems to be based on LCS
+	    //1. Find the LCS
+	    //2. No. of deletion req = str1.length() - lcs.length()
+	    //3. No. of insertion req = str2.lemgth() - lcs.length()
+	    int m=str1.length(), n=str2.length();
+	    int[][] c= new int[m+1][n+1];
+	    for(int i=0;i<=m;i++)
+        c[i][0]=0;
+        for(int j=0;j<=n;j++)
+        c[0][j]=0;
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(str1.charAt(i-1)==str2.charAt(j-1))
+                c[i][j]=c[i-1][j-1]+1;
+                else
+                c[i][j]=Math.max(c[i-1][j],c[i][j-1]);
+            }
+        }
+	    int lcsL= c[m][n];
+	    return (n-lcsL)+(m-lcsL);
+	} 
+	
+}
